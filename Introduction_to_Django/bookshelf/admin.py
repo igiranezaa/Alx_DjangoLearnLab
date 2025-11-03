f18fe765ed1083e70870e01505a7d477fb55f944
Aubin Joanes Igiraneza <aubinigiraneza1@gmail.com>
2025-11-03 16:34:57 +0200

    Fix Task 2: Add admin.py and register Book model with proper admin display

--- Introduction_to_Django/bookshelf/admin.py
+++ Introduction_to_Django/bookshelf/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 from .models import Book
 
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')  # columns to show
-    search_fields = ('title', 'author')                     # add search box
-    list_filter = ('publication_year',)                     # add filter by year
-
-admin.site.register(Book, BookAdmin)
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author')
+    list_filter = ('publication_year',)
